15deaa5400c0f1f4532cb46cab1a7fac8344327b
Ethan Meade <ethanmeade@ucsb.edu>
2024-06-07 15:26:09 -0700

    EM - Modified combine_repo_csvs to filter out features with variance below a threshold

--- combine_repo_csvs.py
+++ combine_repo_csvs.py
@@ -15,12 +15,40 @@ def combine_csv_files(directory_path, output_file):
     
     # Concatenate all dataframes
     combined_csv = pd.concat(csv_files, ignore_index=True)
+
+    print(f"Shape of unprocessed csv: {combined_csv.shape}")
     
-    # Save the combined dataframe to a new CSV file
-    combined_csv.to_csv(output_file, index=False)
-    print(f"Combined CSV saved to {output_file}")
+        # Select only numeric columns for variance calculation
+    numeric_columns = combined_csv.select_dtypes(include=['number']).columns
+    print(f"Number of 'numeric columns': {len(numeric_columns)}")
+    numeric_data = combined_csv[numeric_columns]
+    
+    # Calculate variance of each numeric column
+    variances = numeric_data.var()
+    
+    # Filter out numeric columns with variance below the threshold
+    numeric_columns_to_keep = variances[variances > variance_threshold].index
+
+    print(f"Number of 'numeric columns to keep': {len(numeric_columns_to_keep)}")
+    
+    # Combine the numeric columns to keep with non-numeric columns
+    non_numeric_columns = combined_csv.select_dtypes(exclude=['number']).columns
+
+    print(f"Number of 'non-numeric columns': {len(non_numeric_columns)}")
+    print(f"Non-numeric columns labels: {non_numeric_columns}")
+
+    columns_to_keep = numeric_columns_to_keep.union(non_numeric_columns)
+    
+    filtered_csv = combined_csv[columns_to_keep]
+
+    print(f"Shape of resulting csv: {filtered_csv.shape}")
+    
+    # Save the filtered dataframe to a new CSV file
+    filtered_csv.to_csv(output_file, index=False)
+    print(f"Filtered combined CSV saved to {output_file}")
 
 # Example usage
+variance_threshold = 0.1
 directory_path = '../all_versions/csvs'
-output_file = 'combined_repos_output.csv'
+output_file = 'combined_repos_output_remove_low_var.csv'
 combine_csv_files(directory_path, output_file)
