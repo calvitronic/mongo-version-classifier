bc9e9a6ceb9c19cbd01a6d15f4d6aafc459e30e2
Ethan Meade <ethanmeade@ucsb.edu>
2024-06-09 16:33:16 -0700

    EM - Broke some stuff; moving to another branch and beginning again

--- classifier.py
+++ classifier.py
@@ -3,12 +3,16 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, f1_score, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
+
+import seaborn as sns # Plotting stuff
 
 # Define a random seed so results are reproducable...
-torch_random_seed = 42
+random_seed = 42
 
 # Define the Feedforward Neural Network Model
 class FFN(nn.Module):
@@ -30,19 +34,25 @@ class FFN(nn.Module):
 
 # Custom Dataset
 class CSVDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
-        # Ensure all feature values are numeric
-        # Needs to be modified to take into account feature values are now alphabetic...
-        self.features = torch.from_numpy(self.data.select_dtypes(include=['number']).fillna(0).values)
-        #self.features = self.data.iloc[:, :-5].apply(pd.to_numeric, errors='coerce').fillna(0).values
-        # Ensure all label values are numeric
-        self.labels = F.one_hot(torch.from_numpy(self.data['label'].apply(map_text_to_number).values))
-        # Legacy method of grabbing labels
-        # self.labels = self.data['label'] #.iloc[:, -5:].apply(pd.to_numeric, errors='coerce').fillna(0).values
+    def __init__(self, features_in, labels_in):
+    # def __init__(self, csv_file):
+        # self.data = pd.read_csv(csv_file)
+        # # Ensure all feature values are numeric
+        # # Needs to be modified to take into account feature values are now alphabetic...
+        # self.features = torch.from_numpy(self.data.select_dtypes(include=['number']).fillna(0).values)
+        # #self.features = self.data.iloc[:, :-5].apply(pd.to_numeric, errors='coerce').fillna(0).values
+        # # Ensure all label values are numeric
+        # self.labels = F.one_hot(torch.from_numpy(self.data['label'].apply(map_text_to_number).values))
+        # # Legacy method of grabbing labels
+        # # self.labels = self.data['label'] #.iloc[:, -5:].apply(pd.to_numeric, errors='coerce').fillna(0).values
+        
+        self.features = torch.from_numpy(features_in).float()
+        # self.labels = F.one_hot(torch.from_numpy(labels_in)).long()
+        self.labels = torch.from_numpy(labels_in).long()
 
     def __len__(self):
-        return len(self.data)
+        # return len(self.data)
+        return len(self.labels)
 
     def __getitem__(self, idx):
         #sample = torch.tensor(self.features[idx], dtype=torch.float32)
@@ -61,23 +71,74 @@ def map_text_to_number(text):
     }
     return mapping.get(text, -1)
 
+def get_class_numbers(label_in):
+    label_dict = {
+        "0": 0,
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0
+    }
+    for i in label_in:
+        label_dict[str(i)] += 1
+    return label_dict
+
 # Create the device to use
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
 # Initialize the Dataset and DataLoader
 csv_file = 'Master_CSV_Low_Variance_Removed.csv'  # replace with your CSV file path
-dataset = CSVDataset(csv_file)
+# sns.countplot(x = "label", data=pd.read_csv(csv_file))
+# dataset = CSVDataset(csv_file)
 
-# Split the dataset into training and testing
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-# train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-train_dataset, validation_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.8, 0.05, 0.15])
+dataset = pd.read_csv(csv_file)
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+X = dataset.select_dtypes(include=['number']).fillna(0).values
+y = dataset['label'].apply(map_text_to_number).values
+
+# Splitting dataset into training and testing
+
+trainval_X, X_test, trainval_y, y_test = train_test_split(X, y, test_size=0.15, stratify=y, random_state=random_seed)
+print(f"Shape of trainval_X: {trainval_X.shape}\nShape of trainval_y: {trainval_y.shape}")
+X_train, X_val, y_train, y_val = train_test_split(trainval_X, trainval_y, test_size=0.1, stratify=trainval_y, random_state=random_seed)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
+X_test = scaler.transform(X_test)
+
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_val, y_val = np.array(X_val), np.array(y_val)
+X_test, y_test = np.array(X_test), np.array(y_test)
+
+train_dataset = CSVDataset(X_train, y_train)
+validation_dataset = CSVDataset(X_val, y_val)
+test_dataset = CSVDataset(X_test, y_test)
+
+# Prepare the weighted sampler...
+target_list = []
+for _, t in train_dataset:
+    target_list.append(t)
+
+# print(target_list)
+target_list = torch.tensor(target_list)
+
+class_count = [i for i in get_class_numbers(y_train).values]
+class_weights = 1./torch.tensor(class_count, dtype=torch.float) 
+print(f"Class weights: {class_weights}")
+
+class_weights_all = class_weights[target_list]
+
+weighted_sampler = WeightedRandomSampler(
+    weights=class_weights_all,
+    num_samples=len(class_weights_all),
+    replacement=True
+)
+
+# train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=64, sampler=weighted_sampler)
 val_loader = DataLoader(validation_dataset, batch_size=1)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
 
 # Initialize the Model, Loss Function, and Optimizer
 input_dim = dataset.features.shape[1]  # number of input features
