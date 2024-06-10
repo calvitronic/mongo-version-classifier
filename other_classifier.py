523f3ae84126ca5936db596b77f329d8bbc0cefe
Ethan Meade <ethanmeade@ucsb.edu>
2024-06-09 17:31:41 -0700

    EM - just before adding trustee

--- other_classifier.py
+++ other_classifier.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 import torch
@@ -13,6 +13,8 @@ from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 from sklearn.preprocessing import MinMaxScaler    
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
+
+from trustee import ClassificationTrustee
 
 
 class MulticlassClassification(nn.Module):
@@ -140,7 +142,7 @@ weighted_sampler = WeightedRandomSampler(
     replacement=True
 )
 
-EPOCHS = 25#300
+EPOCHS = 3#15#300
 BATCH_SIZE = 16
 LEARNING_RATE = 0.0007
 NUM_FEATURES = len(X.columns)
@@ -162,3 +164,110 @@ model.to(device)
 criterion = nn.CrossEntropyLoss(weight=class_weights.to(device))
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 # print(model)
+
+def multi_acc(y_pred, y_test):
+    y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
+    _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)    
+    
+    correct_pred = (y_pred_tags == y_test).float()
+    acc = correct_pred.sum() / len(correct_pred)
+    
+    acc = torch.round(acc * 100)
+    
+    return acc
+
+accuracy_stats = {
+    'train': [],
+    "val": []
+}
+loss_stats = {
+    'train': [],
+    "val": []
+}
+
+print("Beginning training...")
+for e in tqdm(range(1, EPOCHS+1)):
+    
+    # TRAINING
+    train_epoch_loss = 0
+    train_epoch_acc = 0
+    model.train()
+    for X_train_batch, y_train_batch in train_loader:
+        X_train_batch, y_train_batch = X_train_batch.to(device), y_train_batch.to(device)
+        optimizer.zero_grad()
+        
+        y_train_pred = model(X_train_batch)
+        
+        train_loss = criterion(y_train_pred, y_train_batch)
+        train_acc = multi_acc(y_train_pred, y_train_batch)
+        
+        train_loss.backward()
+        optimizer.step()
+        
+        train_epoch_loss += train_loss.item()
+        train_epoch_acc += train_acc.item()
+        
+        
+    # VALIDATION    
+    with torch.no_grad():
+        
+        val_epoch_loss = 0
+        val_epoch_acc = 0
+        
+        model.eval()
+        for X_val_batch, y_val_batch in val_loader:
+            X_val_batch, y_val_batch = X_val_batch.to(device), y_val_batch.to(device)
+            
+            y_val_pred = model(X_val_batch)
+                        
+            val_loss = criterion(y_val_pred, y_val_batch)
+            val_acc = multi_acc(y_val_pred, y_val_batch)
+            
+            val_epoch_loss += val_loss.item()
+            val_epoch_acc += val_acc.item()
+    displayed_tloss = train_epoch_loss/len(train_loader)
+    displayed_vloss = val_epoch_loss/len(val_loader)
+    displayed_tacc = train_epoch_acc/len(train_loader)
+    displayed_vacc = val_epoch_acc/len(val_loader)
+    loss_stats['train'].append(displayed_tloss)
+    loss_stats['val'].append(displayed_vloss)
+    accuracy_stats['train'].append(displayed_tacc)
+    accuracy_stats['val'].append(displayed_vacc)
+                              
+    print(f'Epoch {e+0:03}: | Train Loss: {displayed_tloss:.5f} | Val Loss: {displayed_vloss:.5f} | Train Acc: {displayed_tacc:.3f}| Val Acc: {displayed_vacc:.3f}')
+
+# # Create dataframes
+# train_val_acc_df = pd.DataFrame.from_dict(accuracy_stats).reset_index().melt(id_vars=['index']).rename(columns={"index":"epochs"})
+# train_val_loss_df = pd.DataFrame.from_dict(loss_stats).reset_index().melt(id_vars=['index']).rename(columns={"index":"epochs"})
+# # Plot the dataframes
+# fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,7))
+# sns.lineplot(data=train_val_acc_df, x = "epochs", y="value", hue="variable",  ax=axes[0]).set_title('Train-Val Accuracy/Epoch')
+# sns.lineplot(data=train_val_loss_df, x = "epochs", y="value", hue="variable", ax=axes[1]).set_title('Train-Val Loss/Epoch')
+
+# Testing the model:
+y_pred_list = []
+with torch.no_grad():
+    model.eval()
+    for X_batch, _ in test_loader:
+        X_batch = X_batch.to(device)
+        y_test_pred = model(X_batch)
+        _, y_pred_tags = torch.max(y_test_pred, dim = 1)
+        y_pred_list.append(y_pred_tags.cpu().numpy())
+y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+
+confusion_matrix_df = pd.DataFrame(confusion_matrix(y_test, y_pred_list)).rename(columns=idx2class, index=idx2class)
+# heatmap_results = sns.heatmap(confusion_matrix_df, annot=True)
+# plt.show()
+
+print(classification_report(y_test, y_pred_list))
+
+trustee = ClassificationTrustee(expert=model)
+
+trustee.fit(X_train, y_train, num_iter=50, num_stability_iter=10, samples_size=0.2, verbose=True)
+dt, pruned_dt, agreement, reward = trustee.explain()
+dt_y_pred = dt.predict(X_test)
+
+print("Model explanation global fidelity report:")
+print(classification_report(y_pred_list, dt_y_pred))
+print("Model explanation score report:")
+print(classification_report(y_test, dt_y_pred))
